1ac4438511a8033b81bc5b39472beb39a48bcb07
Spencer-MacFarlane <71731663+Spencer-MacFarlane@users.noreply.github.com>
2020-12-11 14:05:33 -0800

    Clarified Ip information

--- Powershell/Ipadapterinfo.ps1
+++ Powershell/Ipadapterinfo.ps1
@@ -1,9 +1,9 @@
- get-ciminstance win32_networkadapterconfiguration |
+get-ciminstance win32_networkadapterconfiguration |
  where-object ipenabled |
   Select-Object description,
-                index,
-                ipaddress,
-                ipsubnet,
-                dnsdomain,
-                dnsserversearchorder| 
-format-table * -autosize
+               index,
+               @{n="ipaddress v4, v6"; e={$_.ipaddress}},
+               ipsubnet,
+               dnsdomain,
+               dnsserversearchorder|
+ format-table * -autosize
