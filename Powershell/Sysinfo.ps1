74b0fae22fadd88f1d81a9d9c82c4ce90fec9e4f
Spencer-MacFarlane <71731663+Spencer-MacFarlane@users.noreply.github.com>
2020-12-16 08:42:07 -0800

    Create Sysinfo.ps1

---
+++ Powershell/Sysinfo.ps1
@@ -0,0 +1,12 @@
+﻿
+$hardwareinfo = Get-CimInstance win32_computersystem|
+                       Select-Object description
+$osinfo = Get-CimInstance win32_operatingsystem|
+                          Select-Object name, 
+                          version
+$processorinfo =  Get-wmiobject win32_processor|
+                  Select-Object  description,
+                  speed, 
+                  L2cachesize,
+                  L3cachesize
+$memoryinfo = Get-WmiObject win32_physicalmemory 
