540a85679db679741f72169bb39e0b8fe3f2acd0
Spencer-MacFarlane <71731663+Spencer-MacFarlane@users.noreply.github.com>
2020-12-19 00:37:27 -0500

    Revert "Revert "shits fucked""
    This reverts commit 1369b9c5929b713d0b15caada1785ed5a4b85890.

---
+++ Powershell/Sysinfobackup.ps1
@@ -0,0 +1,38 @@
+﻿param(
+    [parameter(Mandatory = $false)]
+    [ValidateSet("-System", "-Disk","-Network")]
+    [ValidateNotNull()]
+    [string]$searchparam
+)
+
+Get-Hardwaredesc  = $out0 |out-null 
+Get-OSinfo        = $out1 |out-null 
+Get-ProcessorInfo = $out2 |out-null 
+Get-MemoryInfo    = $out3 |out-null 
+Get-DiskInfo      = $out4 |out-null 
+Get-NetworkInfo   = $out5 |out-null 
+Get-VideoCardInfo = $out6 |out-null 
+$Sysinfo=($out0,$out1,$out2,$out3,$out4,$out5,$out6)
+if ($searchparam -eq "-System") {
+
+   Get-ProcessorInfo
+   Get-OSinfo
+   Get-MemoryInfo
+   Get-VideoCardInfo
+
+}
+elseif ($searchparam -eq "-Disk") {
+
+   Write-Host $sysinfo[4]
+
+}
+elseif ($searchparam -eq "-Network") {
+
+   Write-Host $sysinfo[5]
+}
+else {
+
+    get-SystemSummary
+
+
+}
---
+++ Powershell/Sysinfoimproved.ps1
@@ -0,0 +1,37 @@
+﻿Function Get-Systeminfo{
+param(
+    [parameter(Mandatory = $false)]
+    [ValidateSet("-System", "-Disk","-Network")]
+    [ValidateNotNull()]
+    [string]$NarrowSearch
+)
+
+ 
+# $Sysinfo=($out0,$out1,$out2,$out3,$out4,$out5,$out6)
+
+
+
+if ([string]$Narrowsearch -eq "-System") {
+
+   Get-ProcessorInfo
+   Get-OSinfo
+   Get-MemoryInfo
+   Get-VideoCardInfo
+
+}
+elseif ($Narrowsearch -eq "-Disk") {
+
+   Get-DiskInfo
+
+}
+elseif ($Narrowsearch -eq "-Network") {
+
+   Get-NetworkInfo
+}
+else {
+
+    get-SystemSummary
+
+
+}
+}
